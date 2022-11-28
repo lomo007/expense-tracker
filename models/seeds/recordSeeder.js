451c5e4cbf2f32575cfa0591131ad1aa76fe7eb8
lomo007 <eragon66997@gmail.com>
2022-11-28 16:38:39 +0800

    return pre-logic

--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -5,34 +5,31 @@ const User = require('../user')
 const { records, users } = require('./data')
 const db = require('../../config/mongoose')
 
-db.once('open', async () => { //* async
+db.once('open', () => {
   Promise.all([
-    await users.map(user => { //* await等 User創建完
+    users.map(user => {
       user.password = bcrypt.hashSync(user.password, 10)
       return User.create(user)
     }),
-    Promise.all(
-      records.map((record) => {
-        const { categoryId, userId } = record
-        return Category.findOne({ name: categoryId })
-          .lean()
-          .then(category => {
-            record.categoryId = category._id
-            console.log(userId, record.userId)
-            return User.findOne({ name: record.userId }) //* userId/record.userId
-              .lean()
-              .then(user => {
-                record.userId = user._id //* 錯誤訊息_id of null (非同步 等User創建完)
-                return Record.create(record)
-              })
-              .catch(error => console.log(error))
-          })
-      })
-    )
-  ])
-    .then(() => {
-      console.log('recordSeeds done.')
-      process.exit() // 自動跳轉到下個命令欄 不用再ctrl+c
-    })
+    // 匯入Record 加上userId categoryI
+    Promise.all(records.map((record) => {
+      const { name, date, amount, categoryId, userId } = record
+      return Category.findOne({ name: categoryId })
+        .lean()
+        .then(category => {
+          const categoryId = category._id
+          return User.findOne({ name: userId })
+            .lean()
+            .then(user => {
+              const userId = user._id
+              return Record.create({ name, date, amount, categoryId, userId })
+            })
+            .catch(error => console.log(error))
+        })
+    }))
+  ]).then(() => {
+    console.log('recordSeeds done.')
+    process.exit() // 自動跳轉到下個命令欄 不用再ctrl+c
+  })
     .catch(error => console.log(error))
 })
